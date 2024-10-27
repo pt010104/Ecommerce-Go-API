91cc9222cb8d9e803584f5c519f54ef18dbf6ce8
nguyen2192004 <phamgianguyen.qn@gmail.com>
2024-10-27 17:27:31 +0700

    done verify+getshop for admin

--- internal/admin/delivery/http/handler.go
+++ internal/admin/delivery/http/handler.go
@@ -5,6 +5,23 @@ import (
 	"github.com/pt010104/api-golang/pkg/response"
 )
 
+// VerifyShop godoc
+// @Summary Verify shop
+// @Schemes http https
+// @Description verify shop by  IDs
+// @Tags Shop
+// @Accept json
+// @Produce json
+// @Param Access-Control-Allow-Origin header string false "Access-Control-Allow-Origin" default("*")
+// @Param Authorization header string true "Bearer JWT token" default(Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjAxMTk2NjgsImlhdCI6MTcyODU4MzY2OCwic3ViIjoiNjcwNzgyNWQ0NTgwNGNhYWY4MzE2OTU3Iiwic2Vzc2lvbl9pZCI6InpnSFJMd1NmTnNQVnk2d2g3M0ZLVmpqZXV6T1ZnWGZSMjdRYVd1eGtsdzQ9IiwidHlwZSI6IiIsInJlZnJlc2giOmZhbHNlfQ.Pti0gJ5fO4WjGTsxShGv90pr0E_0jMJdWFEUJYKG4VU)
+// @Param x-client-id header string true "User ID" default(6707825d45804caaf8316957)
+// @Param session-id header string true "Session ID" default(zgHRLwSfNsPVy6wh73FKVjjeuzOVgXfR27QaWuxklw4=)
+// @Param ids body []shop_id
+// @Success 200 {object} VerifyShopResp
+// @Failure 400 {object} response.Resp "Bad Request"
+// @Failure 500 {object} response.Resp "Internal Server Error"
+// @Router /api/v1/admin/verify-shop
+
 func (h handler) VerifyShop(c *gin.Context) {
 	ctx := c.Request.Context()
 
@@ -22,6 +39,9 @@ func (h handler) VerifyShop(c *gin.Context) {
 		response.Error(c, err)
 		return
 	}
+	var verifyShopRes VerifyShopResp
+	res := verifyShopRes.toResList(s)
+
 	h.l.Debugf(ctx, "role:", sc.Role)
-	response.OK(c, s)
+	response.OK(c, res)
 }
--- internal/admin/delivery/http/presenter.go
+++ internal/admin/delivery/http/presenter.go
@@ -1,6 +1,9 @@
 package http
 
-import "github.com/pt010104/api-golang/internal/admin"
+import (
+	"github.com/pt010104/api-golang/internal/admin"
+	"github.com/pt010104/api-golang/internal/models"
+)
 
 type VerifyShopReq struct {
 	ShopID []string `json:"shop_id" binding:"required"`
@@ -10,4 +13,23 @@ func (req VerifyShopReq) toInput() admin.VerifyShopInput {
 	return admin.VerifyShopInput{
 		ShopID: req.ShopID,
 	}
+}
+
+type VerifyShopResp struct {
+	ShopID     string `json:"shop_id"`
+	Name       string `json:"name"`
+	IsVerified bool   `json:"is_verified"`
+}
+
+func (resp VerifyShopResp) toResList(shops []models.Shop) []VerifyShopResp {
+	var resList []VerifyShopResp
+	for _, shop := range shops {
+		res := VerifyShopResp{
+			ShopID:     shop.ID.Hex(),
+			Name:       shop.Name,
+			IsVerified: shop.IsVerified,
+		}
+		resList = append(resList, res)
+	}
+	return resList
 }
