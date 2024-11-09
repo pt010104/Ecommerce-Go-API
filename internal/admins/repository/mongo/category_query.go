ee1170a60af0d484a9fada6876304f9f9803c7ee
pt010104 <hisokaxxzk@gmail.com>
2024-11-09 15:48:00 +0700

    ObjectIDsFromHexOrNil

--- internal/admins/repository/mongo/category_query.go
+++ internal/admins/repository/mongo/category_query.go
@@ -2,6 +2,7 @@ package mongo
 
 import (
 	"github.com/pt010104/api-golang/internal/admins"
+	"github.com/pt010104/api-golang/pkg/mongo"
 
 	"go.mongodb.org/mongo-driver/bson"
 )
@@ -10,7 +11,7 @@ func (repo implRepo) buildCategoryQuery(opt admins.GetCategoriesFilter) bson.M {
 	filter := bson.M{}
 
 	if len(opt.IDs) > 0 {
-		filter["_id"] = bson.M{"$in": opt.IDs}
+		filter["_id"] = bson.M{"$in": mongo.ObjectIDsFromHexOrNil(opt.IDs)}
 	}
 
 	if opt.Name != "" {
