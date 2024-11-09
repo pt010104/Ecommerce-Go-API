5e28210ba23c456ffaf645bfd85a8af5cf39af71
pt010104 <hisokaxxzk@gmail.com>
2024-11-09 13:54:09 +0700

    Add leak

--- internal/admins/uc_interface.go
+++ internal/admins/uc_interface.go
@@ -8,5 +8,7 @@ import (
 
 type UseCase interface {
 	CreateCategory(ctx context.Context, sc models.Scope, input CreateCategoryInput) (models.Category, error)
+	ListCategories(ctx context.Context, sc models.Scope, filter GetCategoriesFilter) ([]models.Category, error)
+
 	VerifyShop(ctx context.Context, sc models.Scope, input VerifyShopInput) ([]models.Shop, error)
 }
