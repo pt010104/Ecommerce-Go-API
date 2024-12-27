2a0d4c4f38a088cf0007587317ced06537b3d62d
pt010104 <hisokaxxzk@gmail.com>
2024-12-27 18:34:55 +0700

    add catch error

--- internal/vouchers/delivery/http/error.go
+++ internal/vouchers/delivery/http/error.go
@@ -9,9 +9,11 @@ var (
 	errNoPermission = pkgErrors.NewHTTPError(140001, "you dont have permision to dothis")
 	errWrongBody    = pkgErrors.NewHTTPError(140002, "Wrong body")
 
-	errExistCode       = pkgErrors.NewHTTPError(140003, "code already exist")
-	errShopNotFound    = pkgErrors.NewHTTPError(140004, "shop not found")
-	ErrVoucherNotFound = pkgErrors.NewHTTPError(140005, "voucher not found")
+	errExistCode                 = pkgErrors.NewHTTPError(140003, "code already exist")
+	errShopNotFound              = pkgErrors.NewHTTPError(140004, "shop not found")
+	ErrVoucherNotFound           = pkgErrors.NewHTTPError(140005, "voucher not found")
+	ErrVoucherExpired            = pkgErrors.NewHTTPError(140006, "voucher expired")
+	ErrVoucherMinimumOrderAmount = pkgErrors.NewHTTPError(140007, "voucher minimum order amount")
 )
 
 func (h handler) mapErrors(e error) error {
@@ -23,6 +25,10 @@ func (h handler) mapErrors(e error) error {
 		return errShopNotFound
 	case vouchers.ErrVoucherNotFound:
 		return ErrVoucherNotFound
+	case vouchers.ErrVoucherExpired:
+		return ErrVoucherExpired
+	case vouchers.ErrVoucherMinimumOrderAmount:
+		return ErrVoucherMinimumOrderAmount
 
 	}
 
